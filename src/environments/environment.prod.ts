9a01b511546335e1bbad205f92f5b3644fcdac7c
temp8101 <temp8101@gmail.com>
2020-04-10 15:47:22 -0400

    test with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    // your web config from step 1
+    apiKey: "AIzaSyAdC6-iPSNbx38uNKlDDjUT_yTR0nLX3Po",
+    authDomain: "cicddemo-7a754.firebaseapp.com",
+    databaseURL: "https://cicddemo-7a754.firebaseio.com",
+    projectId: "cicddemo-7a754",
+    storageBucket: "cicddemo-7a754.appspot.com",
+    messagingSenderId: "273354702810",
+    appId: "1:273354702810:web:20e1c025f37ecda8cdcae3"
+}
 };
